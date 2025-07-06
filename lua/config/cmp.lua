a1cad3f3c943358c72f59c27e86650cad68f8f30
shadiy <lakizvezdas@gmail.com>
2025-07-06 23:31:40 +0200

    init

---
+++ lua/config/cmp.lua
@@ -0,0 +1,13 @@
+
+local cmp = require("cmp")
+
+cmp.setup({
+  mapping = {
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+  },
+})
